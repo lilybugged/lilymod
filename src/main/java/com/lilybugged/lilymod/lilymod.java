fd988848c01db2bd7d0661e1143aaf74a56f66de
Lilianna Swanson <lilianna@swansontech.net>
2014-07-14 20:04:39 -0500

    i don't even
    I did stuff

--- src/main/java/com/lilybugged/lilymod/lilymod.java
+++ src/main/java/com/lilybugged/lilymod/lilymod.java
@@ -1,4 +1,28 @@
 package com.lilybugged.lilymod;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="lilymod",name="lilymod",version="1.7.2-1.0")
 public class lilymod {
+    @Mod.Instance("lilymod")
+    public static lilymod instance;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
 }
